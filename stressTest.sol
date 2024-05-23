8c1b86581f1db4e568b752ee563bee0b6505c095
Awointa <akosawointa@gmail.com>
2024-05-23 15:47:28 +0100

    changed the name of the files from For and while loop to stressTest

---
+++ stressTest.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract ForandWhileLoops{
+    function loops() external pure{
+        for (uint i = 0; i<10; i++){
+            //code
+            if(i==3){
+                continue;
+            }
+            //more code
+            if(i ==5){
+                break;
+            }
+        }
+
+        uint y = 0;    
+        while(y < 10){
+            // code
+            y++;
+        }
+    }
+
+    function sum(uint _n) external pure returns(uint){
+        uint s;
+        for(uint i =1; i <= _n; i++){
+            s += i;
+        }
+        return s;
+    }
+}
