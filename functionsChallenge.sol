258be98b3579641b1280a9fedfa765fbad8cc69f
Awointa <akosawointa@gmail.com>
2024-05-23 21:58:22 +0100

    created sub, mul and div function, compiled and aslo deployed the contract

--- functionsChallenge.sol
+++ functionsChallenge.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.8.7;
 
 contract BasicFunctions{
+    // a function that returns the summation x + y
+    function add(uint x, uint y) external pure returns (uint){
+        return x + y;
+    }
 
+    // this function returns the result of subtracting x - y 
+    function sub(uint x, uint y) external pure returns (uint){
+        return x - y;
+    }
+
+    // this function returns the result of the multiplication x * y 
+    function mul(uint x, uint y) external pure returns  (uint){
+        return x * y;
+    }
+
+    // this function returns a result of dividing x/y
+    function div(uint x, uint y)external pure returns (uint){
+        return x/y;
+    }
 }
