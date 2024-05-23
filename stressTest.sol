7b4c006f254aff9580f87003b6d8ddc82d148490
Awointa <akosawointa@gmail.com>
2024-05-23 16:16:44 +0100

    commented out the code in lesson three

--- stressTest.sol
+++ stressTest.sol
@@ -1,31 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract ForandWhileLoops{
-    function loops() external pure{
-        for (uint i = 0; i<10; i++){
-            //code
-            if(i==3){
-                continue;
-            }
-            //more code
-            if(i ==5){
-                break;
-            }
-        }
 
-        uint y = 0;    
-        while(y < 10){
-            // code
-            y++;
-        }
-    }
 
-    function sum(uint _n) external pure returns(uint){
-        uint s;
-        for(uint i =1; i <= _n; i++){
-            s += i;
+contract counter{
+    uint count;
+    function increment() public returns(uint){
+        for(uint i = 0; i < 1000; i++){
+            count += i;
         }
-        return s;
+        return count;
     }
 }
+
+// contract ForandWhileLoops{
+//     function loops() external pure{
+//         for (uint i = 0; i<10; i++){
+//             //code
+//             if(i==3){
+//                 continue;
+//             }
+//             //more code
+//             if(i ==5){
+//                 break;
+//             }
+//         }
+
+//         uint y = 0;    
+//         while(y < 10){
+//             // code
+//             y++;
+//         }
+//     }
+
+//     function sum(uint _n) external pure returns(uint){
+//         uint s;
+//         for(uint i =1; i <= _n; i++){
+//             s += i;
+//         }
+//         return s;
+//     }
+// }
+
