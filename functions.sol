87a976b626c64dafd6a534648472679962bfe613
Awointa <akosawointa@gmail.com>
2024-05-23 21:31:19 +0100

    created a subtract function

--- functions.sol
+++ functions.sol
@@ -5,4 +5,8 @@ contract functions{
     function add(uint x, uint y)external pure returns (uint){
         return x + y;
     }
+
+    function sub(uint x, uint y) external  pure returns(uint){
+        return x - y;
+    }
 }
